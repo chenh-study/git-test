aa16f8e07d00e7aa6ac654f73799c44012f76952
chenhuang <984015304@qq.com>
2021-05-17 00:49:17 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,10 +2,10 @@ package com.atguigu.git;
 
 /**
  * @author chstart
- * @date 2021/5/17 - 0:39
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
