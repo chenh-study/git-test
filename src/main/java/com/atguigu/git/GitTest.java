fb50c499e610fcf9a2e1f508c2b27269bca36813
chenhuang <984015304@qq.com>
2021-05-17 00:45:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author chstart
+ * @date 2021/5/17 - 0:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
