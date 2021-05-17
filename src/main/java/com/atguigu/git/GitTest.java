5d748d911cf05b7e5bf54f83e946ef29374ffa55
chenhuang <984015304@qq.com>
2021-05-18 00:18:48 +0800

    push commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("master!");
         System.out.println("hot-fix!");
+        System.out.println("push test!");
     }
 }
